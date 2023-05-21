142287a615693050a1cb028f0d72a59f5bc8adb0
AbuQwaider <mohammadabuqwaider@yahoo.com>
2023-05-21 21:32:47 +0300

    First-Update

---
+++ src/MyTestCases.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestCases {
+	@BeforeTest
+	public void MyBeforeTest() {
+		
+		
+		
+	}
+	@Test()
+	public void MyFirstTest()
+	{
+		
+		
+		
+	}
+	@AfterTest
+	public void MyAfterTest()
+	{
+		
+		
+	}
+
+}

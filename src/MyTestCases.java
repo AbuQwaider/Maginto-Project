43f3a8017beaa76bbfc50fc460600578a80e1dac
AbuQwaider <mohammadabuqwaider@yahoo.com>
2023-05-21 21:35:47 +0300

    Second-Update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -6,20 +6,22 @@ public class MyTestCases {
 	@BeforeTest
 	public void MyBeforeTest() {
 		
-		
+	System.out.println("abdalraheem");	
 		
 	}
 	@Test()
 	public void MyFirstTest()
 	{
 		
-		
+		System.out.println("abdalraheem1");	
+
 		
 	}
 	@AfterTest
 	public void MyAfterTest()
 	{
-		
+		System.out.println("abdalraheem2");	
+
 		
 	}
 
